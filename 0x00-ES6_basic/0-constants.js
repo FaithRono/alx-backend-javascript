310e46e07100b013748d680a5562aab5c26b334f
faith rono <faithrono132@gmail.com>
2024-05-28 10:42:41 +0300

    updates

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,20 @@
+// This function declares a constant 'task' with a string value and returns it
 export function taskFirst() {
     const task = 'I prefer const when I can.';
     return task;
-  }
-  
-  export function getLast() {
+}
+
+// This function returns a string ' is okay'
+export function getLast() {
     return ' is okay';
-  }
-  
-  export function taskNext() {
+}
+
+// This function declares a variable 'combination' with a string value,
+// appends the return value of the function 'getLast' to it,
+// and then returns the resulting string
+export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
-  
+
     return combination;
-  }
+}
