ada7b29a1fc7eaf17d218555713e4826cd76e281
faith rono <faithrono132@gmail.com>
2024-05-28 20:51:47 +0300

    updates

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,19 +1,15 @@
-// Function that returns a promise representing a response from an API
 function getFullResponseFromAPI(success) {
-    // Create a new promise
-    const arr = new Promise((resolve, reject) => {
-      // If success is true, resolve the promise with a success message
+    return new Promise((resolve, reject) => {
       if (success) {
-        resolve({ status: 200, body: 'Success' });
+        resolve({
+          status: 200,
+          body: 'Success',
+        });
       } else {
-        // If success is false, reject the promise with an error message
-        reject(new Error('The fake API is not working currently'));
+        reject(Error('The fake API is not working currently'));
       }
     });
-    // Return the promise
-    return arr;
   }
   
-  // Export the function to make it available to other modules
   module.exports = getFullResponseFromAPI;
   
